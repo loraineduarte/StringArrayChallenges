a1b394aef102517c9f0b4aae6727764654116336
Loraine <loraine@ciandt.com>
2020-02-25 14:26:23 -0300

    improvement of code

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,9 @@
 package com;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Main {
 	
@@ -28,20 +30,15 @@ public class Main {
 		System.out.println("fifth Result: " + fifthResult);
 	}
 	
-	private static int solution(int[] firstTest) {
+	private static int solution(int[] list) {
 		int smallest_Positive = 0;
+		Arrays.sort(list);
 		
-		List<Integer> list = new ArrayList<>();
-		for (int j = 0; j < firstTest.length && list.add(firstTest[j]); j++);
-		
-		for (int i = 0; i < list.size(); i++) {
-			//System.out.println("I: " + i);
-			if (list.contains(i)) {
-				//System.out.println("Exists on Array");
-			} else if (list.size()==1) {
+		for (int i = 0; i < list.length; i++) {
+			if (list.length == 1) {
 				smallest_Positive = 1;
 				break;
-			} else {
+			} else if (Arrays.binarySearch(list, i) < 0){
 				if(smallest_Positive == 0){
 					smallest_Positive = i;
 				} else if( i <= smallest_Positive){
@@ -51,9 +48,9 @@ public class Main {
 		}
 		
 		if (smallest_Positive == 0){
-			smallest_Positive = list.size()+1;
+			smallest_Positive = list.length+1;
 		}
-		System.out.println("Smallest: " + smallest_Positive);
+		//System.out.println("Smallest: " + smallest_Positive);
 		return smallest_Positive;
 	}
 }
